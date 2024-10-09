f02dd4f66255c1c314f5e8647b4e43f867c6b5ed
Tommy Nguyen <nguyenanhnhat123456@gmail.com>
2024-10-09 15:49:11 +0700

    Animation for blog page

--- frontend/src/pages/BlogPage/index.tsx
+++ frontend/src/pages/BlogPage/index.tsx
@@ -1,14 +1,20 @@
 import Hero from "./components/Hero";
 import useBlogPage from "./useBlogPage";
 import BlogList from "../../components/BlogList";
+import styled from "styled-components";
 
 const BlogPage = () => {
   const { heroProps, blogListProps } = useBlogPage();
   return (
     <div className="mt-[20px] mobile:mt-[30px] tablet:mt-[50px] *:dark:text-white">
-      <h1 className="text-nowrap border-b-2 border-t-2 text-center font-bold text-h1-lg uppercase text-black-100">
-        the blog
-      </h1>
+      <StyledHeader className="border-b-2 border-t-2">
+        <h1 className="head1 text-nowrap text-center font-bold text-h1-lg uppercase text-black-100 dark:text-white">
+          welcome to the one human mean blog
+        </h1>
+        <h2 className="head2 text-nowrap text-center font-bold text-h1-lg uppercase text-black-100 dark:text-white">
+          welcome to the one human mean blog
+        </h2>
+      </StyledHeader>
       <Hero {...heroProps} />
       <BlogList {...blogListProps} />
     </div>
@@ -16,3 +22,38 @@ const BlogPage = () => {
 };
 
 export default BlogPage;
+
+const StyledHeader = styled.div`
+  min-height: var(--fs-h1-lg);
+  position: relative;
+  overflow: hidden;
+  mask-image: linear-gradient(
+    to right,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 1) 10%,
+    rgba(0, 0, 0, 1) 90%,
+    rgba(0, 0, 0, 0)
+  );
+
+  @keyframes scrollLeft {
+    to {
+      left: max(-8800px, -700vw);
+    }
+  }
+  > * {
+    line-height: calc(1em);
+    position: absolute;
+    left: 100%;
+    animation-name: scrollLeft;
+    animation-duration: 30s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  }
+
+  .head1 {
+    animation-delay: calc(30s / 2 * (2 - 1) * -1);
+  }
+  .head2 {
+    animation-delay: calc(30s / 2 * (2 - 2) * -1);
+  }
+`;
